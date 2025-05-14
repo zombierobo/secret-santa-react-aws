952a8f9a40d77c65e5a84dbe12cd2f757d9a0cb7
Hasmukh Kumar <13313052+zombierobo@users.noreply.github.com>
2025-05-14 18:19:34 +0100

    added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 const client = generateClient<Schema>();
 
-function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  
 
+function App() {
+  const { signOut } = useAuthenticator();
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
   }, []);
-
+  
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
+  }
+  
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
   }
 
   return (
@@ -23,7 +31,10 @@ function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li
+                      onClick={() => deleteTodo(todo.id)}
+            
+            key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
@@ -33,6 +44,8 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
+
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Authenticator>
+      <App />
+    </Authenticator>
   </React.StrictMode>
 );

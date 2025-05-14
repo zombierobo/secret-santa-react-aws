c5602c17839d1428f0b6966f71f3e7dbf20b52e3
Hasmukh Kumar <13313052+zombierobo@users.noreply.github.com>
2025-05-14 21:59:14 +0100

    add missing file

---
+++ src/SecretSantaEventManager/SecretSantaEventManager.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import type { Schema } from "../../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+
+
+const client = generateClient<Schema>();
+
+  
+
+function SecretSantaEventManager() {
+
+  const [events, setEvents] = useState<Array<Schema["Event"]["type"]>>([]);
+  
+  useEffect(() => {
+    client.models.Event.observeQuery().subscribe({
+      next: (data) => setEvents([...data.items]),
+    });
+  }, []);
+  
+  function createEvent() {
+    const eventName = window.prompt("Event Name");
+    eventName && client.models.Event.create({ name: eventName });
+  }
+  
+
+  return (
+    <main>
+      <h1>Your events</h1>
+      <button onClick={createEvent}>+ new</button>
+      <ul>
+        {events.map((event) => (
+          <li
+            
+            key={event.id}>{event.name}</li>
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+export default SecretSantaEventManager;

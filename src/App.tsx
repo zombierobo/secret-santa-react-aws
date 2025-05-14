6263ea54a1f6189734addbdefd4cd44ff902ee68
Hasmukh Kumar <13313052+zombierobo@users.noreply.github.com>
2025-05-14 22:45:31 +0100

    fix lint

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { useAuthenticator } from '@aws-amplify/ui-react';
-import SecretSantaEventManager from "./SecretSantaEventManager/SecretSantaEventManager";
+// import SecretSantaEventManager from "./SecretSantaEventManager/SecretSantaEventManager";
 
 const client = generateClient<Schema>();
 
--- src/SecretSantaEventManager/SecretSantaEventManager.tsx
+++ src/SecretSantaEventManager/SecretSantaEventManager.tsx
@@ -1,28 +1,20 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+// import { useEffect, useState } from "react";
+// import type { Schema } from "../../amplify/data/resource";
+// import { generateClient } from "aws-amplify/data";
 
-
-const client = generateClient<Schema>();
-
-  
+// const client = generateClient<Schema>();
 
 function SecretSantaEventManager() {
-
   // const [events, setEvents] = useState<Array<Schema["Event"]["type"]>>([]);
-  
   // useEffect(() => {
   //   client.models.Event.observeQuery().subscribe({
   //     next: (data) => setEvents([...data.items]),
   //   });
   // }, []);
-  
   // function createEvent() {
   //   const eventName = window.prompt("Event Name");
   //   eventName && client.models.Event.create({ name: eventName });
   // }
-  
-
   // return (
   //   <main>
   //     <h1>Your events</h1>
@@ -30,7 +22,6 @@ function SecretSantaEventManager() {
   //     <ul>
   //       {events.map((event) => (
   //         <li
-            
   //           key={event.id}>{event.name}</li>
   //       ))}
   //     </ul>

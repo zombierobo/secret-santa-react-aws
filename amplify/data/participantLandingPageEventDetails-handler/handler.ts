e4edf12ab46eaed004b4566c43244f581de11105
Hasmukh Kumar <13313052+zombierobo@users.noreply.github.com>
2025-05-15 19:57:56 +0100

    try to fetch event in lambda

--- amplify/data/participantLandingPageEventDetails-handler/handler.ts
+++ amplify/data/participantLandingPageEventDetails-handler/handler.ts
@@ -1,9 +1,17 @@
 import type { Schema } from "../resource";
+import { generateClient } from "aws-amplify/data";
+
+const client = generateClient<Schema>();
 
 export const handler: Schema["participantLandingPageEventDetails"]["functionHandler"] =
   async (event, _context) => {
+    const eventData = await client.models.Event.get(
+      { id: event.arguments.eventId },
+      { authMode: "identityPool" }
+    );
+
     return {
-      organizerName: "Hardcode o",
-      eventName: "Hardcoded event name",
+      organizerName: "",
+      eventName: eventData.data?.name ?? "",
     };
   };

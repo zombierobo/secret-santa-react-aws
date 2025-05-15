50d9d229c5e0e815fbd08a60ff07cc216087cde5
Hasmukh Kumar <13313052+zombierobo@users.noreply.github.com>
2025-05-15 20:07:41 +0100

    add try catch in lambda

--- amplify/data/participantLandingPageEventDetails-handler/handler.ts
+++ amplify/data/participantLandingPageEventDetails-handler/handler.ts
@@ -1,17 +1,23 @@
 import type { Schema } from "../resource";
 import { generateClient } from "aws-amplify/data";
 
-const client = generateClient<Schema>();
-
 export const handler: Schema["participantLandingPageEventDetails"]["functionHandler"] =
   async (event, _context) => {
-    const eventData = await client.models.Event.get(
-      { id: event.arguments.eventId },
-      { authMode: "identityPool" }
-    );
+    let eventName = "";
+    try {
+      const client = generateClient<Schema>();
+      const eventData = await client.models.Event.get(
+        { id: event.arguments.eventId },
+        { authMode: "identityPool" }
+      );
+      eventName = eventData.data?.name ?? "",;
+    } catch (err) {
+      eventName = "error occurred";
+      console.error("failed to fetch event details", err);
+    }
 
     return {
       organizerName: "",
-      eventName: eventData.data?.name ?? "",
+      eventName: eventName
     };
   };

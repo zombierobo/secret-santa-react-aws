e27b80a669160a282fcaf0615ff33c513e594e43
Hasmukh Kumar <13313052+zombierobo@users.noreply.github.com>
2025-05-15 21:57:44 +0100

    add function backend def

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,10 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+import { data } from "./data/resource";
+import { participantLandingPageDataFetcher } from "./functions/participant-landing-page-data-fetcher/resource";
 
 defineBackend({
   auth,
   data,
+  participantLandingPageDataFetcher,
 });

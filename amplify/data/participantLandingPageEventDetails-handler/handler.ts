0c55f577e1416670f471f2e4272bfed13c6ec3d1
Hasmukh Kumar <13313052+zombierobo@users.noreply.github.com>
2025-05-15 20:26:59 +0100

    try a fix for lambda

--- amplify/data/participantLandingPageEventDetails-handler/handler.ts
+++ amplify/data/participantLandingPageEventDetails-handler/handler.ts
@@ -1,5 +1,8 @@
 import type { Schema } from "../resource";
+import { Amplify } from "aws-amplify";
+import outputs from "../../../amplify_outputs.json";
 import { generateClient } from "aws-amplify/data";
+Amplify.configure(outputs);
 
 export const handler: Schema["participantLandingPageEventDetails"]["functionHandler"] =
   async (event, _context) => {

409ce0669adddff0ef0e77d673abdb7face86bcc
Hasmukh Kumar <13313052+zombierobo@users.noreply.github.com>
2025-05-15 21:45:24 +0100

    second try to get lambda working

--- amplify/functions/participant-landing-page-data-fetcher/handler.ts
+++ amplify/functions/participant-landing-page-data-fetcher/handler.ts
@@ -1,29 +1,29 @@
 import { type Schema } from "../../data/resource";
-// import { Amplify } from "aws-amplify";
-// import { generateClient } from "aws-amplify/data";
-// import { getAmplifyDataClientConfig } from "@aws-amplify/backend/function/runtime";
-// import { env } from "$amplify/env/participant-landing-page-data-fetcher"; // the import is '$amplify/env/<function-name>'
+import { Amplify } from "aws-amplify";
+import { generateClient } from "aws-amplify/data";
+import { getAmplifyDataClientConfig } from "@aws-amplify/backend/function/runtime";
+import { env } from "$amplify/env/participant-landing-page-data-fetcher"; // the import is '$amplify/env/<function-name>'
 
-// const { resourceConfig, libraryOptions } =
-//   await getAmplifyDataClientConfig(env);
+const { resourceConfig, libraryOptions } =
+  await getAmplifyDataClientConfig(env);
 
-// Amplify.configure(resourceConfig, libraryOptions);
+Amplify.configure(resourceConfig, libraryOptions);
 
-// const client = generateClient<Schema>();
+const client = generateClient<Schema>();
 
 export const handler: Schema["participantLandingPageDataFetcher"]["functionHandler"] =
   async (event, _context) => {
     let eventName = "";
-    // try {
-    //   const eventData = await client.models.Event.get(
-    //     { id: event.arguments.eventId },
-    //     { authMode: "identityPool" }
-    //   );
-    //   eventName = eventData.data?.name ?? "";
-    // } catch (err) {
-    //   eventName = "error occurred";
-    //   console.error("failed to fetch event details", err);
-    // }
+    try {
+      const eventData = await client.models.Event.get(
+        { id: event.arguments.eventId },
+        { authMode: "identityPool" }
+      );
+      eventName = eventData.data?.name ?? "";
+    } catch (err) {
+      eventName = "error occurred";
+      console.error("failed to fetch event details", err);
+    }
 
     return {
       organizerName: "",
